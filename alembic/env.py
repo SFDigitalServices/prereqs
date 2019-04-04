049b97e7203be60e6243731c512ca0faf939042b
Josh Chou <626661+josh-chou@users.noreply.github.com>
2019-04-04 09:52:14 -0700

    add print to debug

--- alembic/env.py
+++ alembic/env.py
@@ -42,6 +42,7 @@ def run_migrations_offline():
 
     """
     url = get_url()
+    print("the url is ", url)
     context.configure(
         url=url, target_metadata=target_metadata, literal_binds=True
     )
